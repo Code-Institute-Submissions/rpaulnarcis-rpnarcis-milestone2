311baeccb4700512c60cada18b0be4a33bcc324a
rpaulnarcis <rpaulnarcis@gmail.com>
2020-12-04 10:20:54 +0000

    Removed the banner on the aboutus html page the css for it as well, fixed the code for markers on maps in the main js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,191 +1,102 @@
-function initMap(){
-   var map = new google.maps.Map(document.getElementById('map'), {
-       zoom: 12,
-       center: {
-           lat:18.4209,
-           lng:-64.6167
-       }
-   });
-
-
-var labels ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-var locations = [
-    {lat: 18.4208, lng: -64.6181},
-    {lat: 18.4194, lng: -64.6186},
-    {lat: 18.4190, lng: -64.6186},
-    {lat: 18.4221, lng: -64.6186}
-   
-];
-
-var markers = locations.map(function(location, i) {
-    return new google.maps.Marker({
-        position: location,
-        map: map,
-        label: labels[i % labels.length]
-    });
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom:12,
+        center: { lat:18.4209, lng:-64.6167 },
 });
 
+const pos1 = {lat: 18.41945, lng: -64.61857};
+const marker1 = new google.maps.Marker({position: pos1, map: map});
+const pos2 = {lat: 18.42215, lng: -64.61852};
+const marker2 = new google.maps.Marker({position: pos2, map: map});
+const pos3 = {lat: 18.42082, lng: -64.61808};
+const marker3 = new google.maps.Marker({position: pos3, map: map});
+const pos4 = {lat: 18.41902, lng: -64.61858};
+const marker4 = new google.maps.Marker({position: pos4, map: map});
 
- var map1 = new google.maps.Map(document.getElementById('map1'), {
-       zoom: 12,
-       center: {
-           lat:18.5166,
-           lng:-77.8828
-       }
-   });
-
-   var labels ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-var locations = [
-    {lat: 18.5145, lng: -77.8813},
-    {lat: 18.5148, lng: -77.8808},
-    {lat: 18.5143, lng: -77.8806},
-    {lat: 18.5157, lan: -77.8814}
-   
-];
-
-var markers1 = locations.map(function(location, i) {
-    return new google.maps.Marker({
-        position: location,
-        map: map1,
-        label: labels[i % labels.length]
-    });
+const map1 = new google.maps.Map(document.getElementById("map1"), {
+        zoom:12,
+        center: { lat:18.5166, lng:-77.8828 },
 });
 
-var map2 = new google.maps.Map(document.getElementById('map2'), {
-       zoom: 12,
-       center: {
-           lat:13.0639,
-           lng:-59.5645
-       }
-   });
+const pos5 = {lat: 18.51449, lng: -77.88126};
+const marker5 = new google.maps.Marker({position: pos5, map: map1});
+const pos6 = {lat: 18.51477, lng: -77.88083};
+const marker6 = new google.maps.Marker({position: pos6, map: map1});
+const pos7 = {lat: 18.51436, lng: -77.88057};
+const marker7 = new google.maps.Marker({position: pos7, map: map1});
+const pos8 = {lat: 18.50767, lng: -77.89627};
+const marker8 = new google.maps.Marker({position: pos8, map: map1});
 
-   var labels ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-var locations = [
-    {lat: 13.0669, lng: -59.5712},
-    {lat: 13.0666, lng: -59.5688},
-    {lat: 13.0674, lng: -59.5695}
-    
-   
-];
-
-var markers = locations.map(function(location, i) {
-    return new google.maps.Marker({
-        position: location,
-        map: map2,
-        label: labels[i % labels.length]
-    });
+const map2 = new google.maps.Map(document.getElementById("map2"), {
+        zoom:12,
+        center: { lat:13.0639, lng:-59.5645 },
 });
 
-var map3 = new google.maps.Map(document.getElementById('map3'), {
-       zoom: 12,
-       center: {
-           lat:19.3394,
-           lng:-81.3810
-       }
-   });
+const pos9 = {lat: 13.06690, lng: -59.57124};
+const marker9 = new google.maps.Marker({position: pos9, map: map2});
+const pos10 = {lat: 13.0667, lng: -59.56879};
+const marker10 = new google.maps.Marker({position: pos10, map: map2});
+const pos11 = {lat: 13.06742, lng: -59.56953};
+const marker11 = new google.maps.Marker({position: pos11, map: map2});
+const pos12 = {lat: 13.06746, lng: -59.56937};
+const marker12 = new google.maps.Marker({position: pos12, map: map2});
 
-   var labels ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-var locations = [
-    {lat: 19.3313, lng: -81.3815},
-    {lat: 19.3312, lng: -81.3809},
-    {lat: 19.3318, lng: -81.3809},
-    {lat: 19.3318, lng: -81.3807}
-    
-   
-];
-
-var markers = locations.map(function(location, i) {
-    return new google.maps.Marker({
-        position: location,
-        map: map3,
-        label: labels[i % labels.length]
-    });
+const map3 = new google.maps.Map(document.getElementById("map3"), {
+        zoom:12,
+        center: { lat:19.3394, lng:-81.3810 },
 });
 
-var map4 = new google.maps.Map(document.getElementById('map4'), {
-       zoom: 12,
-       center: {
-           lat:14.6023,
-           lng:-61.0731
-       }
-   });
+const pos13 = {lat: 19.33123, lng: -81.38091};
+const marker13 = new google.maps.Marker({position: pos13, map: map3});
+const pos14 = {lat: 19.33131, lng: -81.38153};
+const marker14 = new google.maps.Marker({position: pos14, map: map3});
+const pos15 = {lat: 19.33183, lng: -81.38095};
+const marker15 = new google.maps.Marker({position: pos15, map: map3});
+const pos16 = {lat: 19.33182, lng: -81.38067};
+const marker16 = new google.maps.Marker({position: pos16, map: map3});
 
-   var labels ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-var locations = [
-    {lat: 14.6023, lng: -61.0728},
-    {lat: 14.6022, lng: -61.0729},
-    {lat: 14.6022, lng: -61.0727}
-    
-   
-];
-
-var markers = locations.map(function(location, i) {
-    return new google.maps.Marker({
-        position: location,
-        map: map4,
-        label: labels[i % labels.length]
-    });
+const map4 = new google.maps.Map(document.getElementById("map4"), {
+        zoom:12,
+        center: { lat:14.6023, lng:-61.0731 },
 });
 
-var map5 = new google.maps.Map(document.getElementById('map5'), {
-       zoom: 12,
-       center: {
-           lat:12.5159,
-           lng:-70.0365
-       }
-   });
+const pos17 = {lat: 14.60232, lng: -61.07284};
+const marker17 = new google.maps.Marker({position: pos17, map: map4});
+const pos18 = {lat: 14.60218, lng: -61.07293};
+const marker18 = new google.maps.Marker({position: pos18, map: map4});
+const pos19 = {lat: 14.60221, lng: -61.07274};
+const marker19 = new google.maps.Marker({position: pos19, map: map4});
+const pos20 = {lat: 14.60360, lng: -61.07242};
+const marker20 = new google.maps.Marker({position: pos20, map: map4});
 
-   var labels ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-var locations = [
-    {lat: 12.5165, lng: -70.0372},
-    {lat: 12.5168, lng: -70.0369},
-    {lat: 12.5168, lng: -70.0371}
-    
-   
-];
-
-var markers = locations.map(function(location, i) {
-    return new google.maps.Marker({
-        position: location,
-        map: map5,
-        label: labels[i % labels.length]
-    });
+const map5 = new google.maps.Map(document.getElementById("map5"), {
+        zoom:12,
+        center: { lat:12.5159, lng:-70.0365 },
 });
 
-var map6 = new google.maps.Map(document.getElementById('map6'), {
-       zoom: 12,
-       center: {
-           lat:11.1576,
-           lng:-60.8388
-       }
-   });
+const pos21 = {lat: 12.51682, lng: -70.03688};
+const marker21 = new google.maps.Marker({position: pos21, map: map5});
+const pos22 = {lat: 12.51650, lng: -70.03723};
+const marker22 = new google.maps.Marker({position: pos22, map: map5});
+const pos23 = {lat: 12.51682, lng: -70.03713};
+const marker23 = new google.maps.Marker({position: pos23, map: map5});
+const pos24 = {lat: 12.51685, lng: -70.03708};
+const marker24 = new google.maps.Marker({position: pos24, map: map5});
 
-   var labels ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-var locations = [
-    {lat: 11.15715, lng: -60.8375},
-    {lat: 11.1575, lng: -60.8368},
-    {lat: 11.1587, lng: -60.8364}
-    
-   
-];
-
-var markers = locations.map(function(location, i) {
-    return new google.maps.Marker({
-        position: location,
-        map: map6,
-        label: labels[i % labels.length]
-    });
+const map6 = new google.maps.Map(document.getElementById("map6"), {
+        zoom:12,
+        center: { lat:11.1576, lng:-60.8388 },
 });
-var markerCluster =  new MarkerClusterer(map6, markers,   
-    {imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"});
-   
-}
 
+const pos25 = {lat: 11.15782, lng: -60.83649};
+const marker25 = new google.maps.Marker({position: pos25, map: map6});
+const pos26 = {lat: 11.15784, lng: -60.83629};
+const marker26 = new google.maps.Marker({position: pos26, map: map6});
+const pos27 = {lat: 11.15729, lng: -60.83706};
+const marker27 = new google.maps.Marker({position: pos27, map: map6});
+const pos28 = {lat: 11.15823, lng: -60.83729};
+const marker28 = new google.maps.Marker({position: pos28, map: map6});
+ 
+};
 
+  

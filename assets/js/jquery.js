393ad44762bb77ea31218b055c1369f499e2ccf9
rpaulnarcis <rpaulnarcis@gmail.com>
2020-12-07 09:08:44 +0000

    Added a video folder and uploaded a video

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -4,3 +4,32 @@ $(document) .ready(function() {
     });
 });
 
+$(function(){
+    $("#add_name").validate({
+        rules: {
+            ex_model: {
+                required: true,
+                minlength: 8
+            },
+            ex_serial: "required"
+        },
+        messages: {
+            ex_model: {
+                required: "Please enter  modaldata",
+                minlength: "Your data must be at least 8 characters"
+            },      
+            ex_serial: "Please enter some data"
+        }
+    })
+
+    $("#subBtn").on("click", function(){
+        if($("#add_name").valid()){
+            //alert("success");
+            $("#myModal").modal("show");
+        } else {
+            //alert("Values are not entered");
+            //whatever you want to do when values are not entered
+        }
+        return false;
+    });
+})
